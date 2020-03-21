644dd96ee67055da689cd267e85fc0faff5dee87
Felix Nordén <felixnorden@gmail.com>
2020-03-21 16:42:09 +0100

    Add wrapper base class for api v3

---
+++ src/wrapper.py
@@ -0,0 +1,44 @@
+"""IGDB wrapper module for the api v3 with Apicalypse"""
+
+from requests import get
+import igdbapi_pb2 as igdb
+
+API_URL = "https://api-v3.igdb.com/"
+
+class IGDBWrapper:
+    def __init__(self, user_key=None):
+        self.user_key = user_key
+        self.endpoint = ''
+
+    def api_request(self, endpoint=None, query=None):
+        """
+        Takes an endpoint and the Apicalypse query and returns the api response as a byte string.
+        """
+        url = IGDBWrapper._build_url(endpoint)
+        params = self._compose_request(query)
+
+        response = get(url, **params)
+        response.raise_for_status()
+
+        return response.content
+
+    @staticmethod
+    def _build_url(endpoint=''):
+        return f'{API_URL}{endpoint}'
+
+    def _compose_request(self, query=None):
+        if not query:
+            raise Exception('No query provided!\nEither provide an inline query following Apicalypse\'s syntax or an Apicalypse object')
+       
+        request_params = {
+            'headers': {
+                # 'Accept': 'application/json',
+                'user-key': self.user_key
+            }
+        }
+
+        if isinstance(query, str):
+            request_params['data'] = query
+            return request_params
+
+        raise TypeError('Incorrect type of argument \'query\', only Apicalypse-like strings or Apicalypse objects are allowed')
---
+++ tests/test_wrapper.py
@@ -0,0 +1,30 @@
+from pytest import raises
+from src.wrapper import IGDBWrapper
+
+def test_stores_user_key():
+    wrapper = IGDBWrapper('test')
+    assert hasattr(wrapper, 'user_key')
+    assert wrapper.user_key == 'test'
+
+def test_composes_query():
+    wrapper = IGDBWrapper('test')
+    assert IGDBWrapper._build_url('dummy') == 'https://api-v3.igdb.com/dummy'
+    assert wrapper._compose_request('fields test,test2,test3; offset 2') == {
+        'data': 'fields test,test2,test3; offset 2',
+        'headers': {
+            'Accept': 'application/json',
+            'user-key': 'test'
+        }
+    }
+
+def test_raises_when_query_bad():
+    wrapper = IGDBWrapper('')
+    with raises(Exception) as exc:
+        wrapper._compose_request()
+        assert exc.type is Exception
+        assert exc.value.args[0] == 'No query provided!\nEither provide an inline query following Apicalypse\'s syntax or an Apicalypse object'
+
+    with raises(TypeError) as exc:
+        wrapper._compose_request(11)
+        assert exc.type is TypeError
+        assert exc.value.args[0] == 'Incorrect type of argument \'query\', only Apicalypse-like strings or Apicalypse objects are allowed'
