f00a27a9f09fa269c89fcb34e40059edba1a10c8
Felix Nordén <felixn@twitch.tv>
2020-03-24 18:26:15 +0100

    Update package information

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
     
 setup(
   name="igdb-api-python",
-  version="0.0.1",
+  version="0.0.4",
   author="Felix Nordén",
   author_email="felixnorden@gmail.com",
-  description=:"An API wrapper for IGDB API v3",
+  description="An API wrapper for IGDB API v3",
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/igdb/igdb-api-python/",
@@ -18,6 +18,7 @@ setup(
       "Operating System :: OS Independent",
   ],
   python_requires='>=3',
-  packages=find_packages(),
+  packages=['igdb'],
+  package_dir={'igdb': 'src/igdb'},
   install_requires=['requests']
 )
